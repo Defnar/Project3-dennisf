fc23ccff8a068cccf5fcd21c6918376239e7c5de
Dennis <dennis-farris@hotmail.com>
2025-09-02 13:20:00 -0400

    implemented task middlewares

--- auth/middleware.js
+++ auth/middleware.js
@@ -1,5 +1,7 @@
 import { Project } from "../models/Project.js";
+import { Task } from "../models/Task.js";
 
+//project middlewares
 export const authEditMiddleware = async (req, res, next) => {
   try {
     const project = await Project.findById(req.params.projectId);
@@ -15,6 +17,31 @@ export const authEditMiddleware = async (req, res, next) => {
 
 export const checkProjectMiddleware = (req, res, next) => {
   if (!req.project)
-    res.status(403).json({ message: "Unauthorized to interact with project" });
-next();
+    return res
+      .status(403)
+      .json({ message: "Unauthorized to interact with project" });
+  next();
+};
+
+//task middlewares
+
+export const taskMiddleware = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.taskId);
+
+    if (task && task.project === req.params.projectId) req.task = task;
+
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+export const checkTaskMidleware = (req, res, next) => {
+  if (!req.task)
+    return res
+      .status(403)
+      .json({ message: "Unauthorized to interact with task" });
+  next();
 };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,5 +1,6 @@
 import e from "express";
 import taskControllers from "../controllers/taskControllers.js";
+import { taskMiddleware } from "../auth/middleware.js";
 const router = e.Router();
 
 //api/projects/:id/tasks/
@@ -7,5 +8,6 @@ const router = e.Router();
 router.post("/", taskControllers.createTask);
 router.get("/", taskControllers.getTasks);
 
+router.use(taskMiddleware);
 
 export default router;
