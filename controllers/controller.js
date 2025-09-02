bb1348d76cfd16e8072dc1011fbbc2543093284e
Dennis <dennis-farris@hotmail.com>
2025-09-02 15:59:34 -0400

    single controller setup completed?

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,9 +7,9 @@ const create = (Model) => async (req, res) => {
 
     if (req.project) newModel.project = req.project._id;
 
-    const model = await Model.create(req.body);
+    const model = await Model.create(newModel);
 
-    res.status(201).json({ message: `${Model} successfully created`, model });
+    res.status(201).json({ message: `${Model.modelName} successfully created`, model });
   } catch (err) {
     console.log(err);
     res.status(500).json({ err: err.message });
@@ -25,6 +25,8 @@ const getAll = (Model) => async (req, res) => {
 
     if (models.length === 0)
       return res.status(404).json({ message: "No resources found" });
+
+    res.send(models);
   } catch (err) {
     console.log(err);
     res.status(500).json({ err: err.message });
@@ -33,10 +35,42 @@ const getAll = (Model) => async (req, res) => {
 
 const getOne = (Model) => async (req, res) => {
   try {
-    if (req.task) res.json(req.task);
-    else res.json(req.project);
+    res.json(req[Model.modelName.toLowerCase()])
   } catch (err) {
     res.status(500).json({ error: message.err });
     console.log(err);
   }
 };
+
+const edit = (Model) => async (req, res) => {
+    try {
+        if (!req.body)
+            return res.status(400).json({message: "Body cannot be empty"});
+
+        const model = Object.assign(req.task || req.project, req.body);
+
+        await model.save();
+        res.json({message: `${Model.modelName} successfully updated`, model})
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: err.message})
+    }
+}
+
+const deleteOne = (Model) => async (req, res) => {
+    try {
+        await (req.task || req.project).deleteOne();
+        res.json({ message: `${Model.modelName} successfully deleted`})
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: err.message})
+    }
+};
+
+export default {
+    create,
+    getAll,
+    getOne,
+    edit,
+    deleteOne
+}
