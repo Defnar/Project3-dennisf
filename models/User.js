f0901ab412d208da1a47515940c429c0c2c68696
Dennis <dennis-farris@hotmail.com>
2025-09-02 09:59:30 -0400

    user model set up

---
+++ models/User.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, "username required"],
+    min: 6,
+  },
+  password: {
+    type: String,
+    required: [true, "password required"],
+    min: 8,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "email required"],
+    match: [/.+\@.+\..+/, "please provide a valid email"],
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+UserSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+export const User = model("User", UserSchema);
